4a62f3f23ce8753fbb9312224f6448b6afa8355c
corvalol4ic <139862833+corvalol4ic@users.noreply.github.com>
2025-05-01 20:22:52 +0300

    Update start.js

--- handlers/start.js
+++ handlers/start.js
@@ -1,5 +1,5 @@
 module.exports = (bot) => {
     bot.command('start', async (ctx) => {
-      await ctx.reply('Добро пожаловать! Я ваш Telegram-бот.');
+      await ctx.reply('Добро пожаловать! Я ваш Telegram-бот. ');
     });
   };
